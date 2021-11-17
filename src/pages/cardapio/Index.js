7af8b2e74f441bee58c2d7d8a45a7a82455cd729
MatheusR1 <matheusrocha_456@hotmail.com>
2021-11-16 22:04:39 -0300

    mundaças que nao me lembro o porq

--- src/pages/cardapio/Index.js
+++ src/pages/cardapio/Index.js
@@ -1,13 +1,14 @@
 import React, { useState, useEffect } from "react";
 import { Text, View, FlatList } from "react-native";
 import api from '../../services/api';
-import { Card, Button, Icon } from "react-native-elements";
-import {useCart} from '../../context/CartProvider';
+import { Card, Button, Icon, SearchBar } from "react-native-elements";
+import { useCart } from '../../context/CartProvider';
 
 export default function Cardapio() {
 
     const [menu, setMenu] = useState();
-    const {addCart} = useCart();
+    const { addCart } = useCart();
+    const [search, setSearch] = useState();
 
     useEffect(() => {
         try {
@@ -15,14 +16,21 @@ export default function Cardapio() {
         } catch (error) {
             console.log(error)
         }
-    },[]);
+    }, []);
 
     const getCardapio = async () => {
         const response = await api.get('/menu');
         let produtos = response.data?.produtos;
         setMenu(produtos);
     };
-    
+
+    const searchProduto = async (value) => {
+        setSearch(value)
+        const response = await api.get(`/menu?produtos.nome=${search}`);
+        let produtos = response.data;
+        console.log('asoidjasoijasd',produtos)
+    }
+
     const cards = (produto) => {
         return (
             <Card>
@@ -32,7 +40,7 @@ export default function Cardapio() {
                 <Button
                     icon={<Icon type='font-awesome' name='plus' color='#ffffff' />}
                     buttonStyle={{ borderRadius: 0, marginLeft: 0, marginRight: 0, marginBottom: 0 }}
-                    onPress = {()=>addCart(produto)}
+                    onPress={() => addCart(produto)}
                 />
             </Card>
         )
@@ -40,8 +48,13 @@ export default function Cardapio() {
 
     return (
         <View>
-            <Text> hello cardapio </Text>
+            <SearchBar
+                onChangeText={(value) => searchProduto(value)}
+                value={search}
+                placeholder="procure um produto"
+            />
             <FlatList
+                contentContainerStyle={{ paddingBottom: 100 }}
                 keyExtracto={item => item.id}
                 data={menu}
                 renderItem={({ item }) => cards(item)}
